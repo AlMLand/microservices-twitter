bbe657f538532b98262e43c25ce387d90987152e
Alex Morland <morlandalex@googlemail.com>
2022-12-15 23:56:19 +0100

    code refactoring in TwitterV2StreamHelper -> SRP

--- twitter-to-kafka-service/src/main/kotlin/com/AlMLand/runner/TwitterV2StreamHelper.kt
+++ twitter-to-kafka-service/src/main/kotlin/com/AlMLand/runner/TwitterV2StreamHelper.kt
@@ -2,14 +2,11 @@ package com.AlMLand.runner
 
 import com.AlMLand.config.TwitterProperties
 import com.AlMLand.listener.TwitterStatusListener
-import com.AlMLand.runner.HttpRequest.GET
-import com.AlMLand.runner.HttpRequest.POST
+import com.AlMLand.runner.HttpRequest.*
+import org.apache.http.HttpEntity
 import org.apache.http.client.config.CookieSpecs
 import org.apache.http.client.config.RequestConfig
-import org.apache.http.client.methods.HttpEntityEnclosingRequestBase
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.client.methods.HttpPost
-import org.apache.http.client.methods.HttpRequestBase
+import org.apache.http.client.methods.*
 import org.apache.http.client.utils.URIBuilder
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.CloseableHttpClient
@@ -41,7 +38,7 @@ private const val DEFAULT_CHARSET = "UTF-8"
 private const val BODY_ADD_TEMPLATE = "{\"add\": [%s]}"
 private const val BODY_DELETE_TEMPLATE = "{\"delete\":{\"ids\":[%s]}}"
 
-enum class HttpRequest { GET, POST }
+enum class HttpRequest { GET, POST, DEFAULT }
 
 @Component
 @ConditionalOnExpression("\${twitter-to-kafka-service.enable-v2-tweets} && not \${twitter-to-kafka-service.enable-mock-tweets}")
@@ -51,11 +48,8 @@ open class TwitterV2StreamHelper(
 ) {
 
     fun connectStream(bearerToken: String) {
-        val httpClient = getHttpClient()
-        val httpGet = createHttpGet(bearerToken)
-        val httpResponse = httpClient.execute(httpGet)
-        val entity = httpResponse.entity
-        entity?.let {
+        val entity = getHttpResponseEntity(bearerToken)
+        entity.let {
             val reader = BufferedReader(InputStreamReader(entity.content))
             var line = reader.readLine()
             while (line != null) {
@@ -84,9 +78,10 @@ open class TwitterV2StreamHelper(
     }
 
     private fun createRules(bearerToken: String, rules: Map<String, String>) {
-        val httpClient = getHttpClient()
-        val httpPost = createHttpRequest(POST, bearerToken) as HttpEntityEnclosingRequestBase
-        val body = StringEntity(getFormattedString(BODY_ADD_TEMPLATE, rules))
+        val (httpClient, httpPost, body) = getRequestBodyHttpRequestHttpClient(
+            bearerToken,
+            rules = rules
+        )
         httpPost.entity = body
         val response = httpClient.execute(httpPost)
         val entity = response.entity
@@ -94,21 +89,32 @@ open class TwitterV2StreamHelper(
     }
 
     private fun deleteRules(bearerToken: String, existingRules: List<String>) {
-        val httpClient = getHttpClient()
-        val httpPost = createHttpRequest(POST, bearerToken) as HttpEntityEnclosingRequestBase
-        val body = StringEntity(getFormattedString(BODY_DELETE_TEMPLATE, existingRules))
+        val (httpClient, httpPost, body) = getRequestBodyHttpRequestHttpClient(
+            bearerToken,
+            existingRules = existingRules
+        )
         httpPost.entity = body
         val response = httpClient.execute(httpPost)
         val httpEntity = response.entity
         httpEntity?.let { println(EntityUtils.toString(httpEntity, DEFAULT_CHARSET)) }
     }
 
-    private fun getRules(bearerToken: String): List<String> {
+    private fun getRequestBodyHttpRequestHttpClient(
+        bearerToken: String,
+        rules: Map<String, String>? = null,
+        existingRules: List<String>? = null
+    ): Triple<CloseableHttpClient, HttpEntityEnclosingRequestBase, StringEntity> {
         val httpClient = getHttpClient()
-        val httpGet = createHttpRequest(GET, bearerToken)
-        val httpResponse = httpClient.execute(httpGet)
-        val entity = httpResponse.entity
-        entity?.let {
+        val httpPost = createHttpRequest(POST, bearerToken) as HttpEntityEnclosingRequestBase
+        val body = StringEntity(rules?.let { getFormattedString(rules) } ?: existingRules?.let {
+            getFormattedString(existingRules)
+        })
+        return Triple(httpClient, httpPost, body)
+    }
+
+    private fun getRules(bearerToken: String): List<String> {
+        val entity = getHttpResponseEntity(bearerToken, GET)
+        entity.let {
             val json = JSONObject(EntityUtils.toString(entity, DEFAULT_CHARSET))
             if (json.length() > 1 && json.has("data")) {
                 val jsonArray = json.get("data") as JSONArray
@@ -120,7 +126,18 @@ open class TwitterV2StreamHelper(
         return emptyList()
     }
 
-    private fun getFormattedString(template: String, ids: List<String>): String {
+    private fun getHttpResponseEntity(bearerToken: String, httpRequest: HttpRequest = DEFAULT): HttpEntity {
+        val httpClient = getHttpClient()
+        val request = when (httpRequest) {
+            DEFAULT -> createHttpGet(bearerToken)
+            GET -> createHttpRequest(GET, bearerToken)
+            else -> throw IllegalArgumentException("Not allowed argument as request")
+        }
+        val httpResponse = httpClient.execute(request)
+        return httpResponse.entity
+    }
+
+    private fun getFormattedString(ids: List<String>): String {
         val result = when (ids.size) {
             1 -> "\"${ids[0]}\""
             else -> {
@@ -131,14 +148,14 @@ open class TwitterV2StreamHelper(
                 sb.substring(0, sb.length - 1)
             }
         }
-        return String.format(template, result)
+        return String.format(BODY_DELETE_TEMPLATE, result)
     }
 
-    private fun getFormattedString(template: String, rules: Map<String, String>): String {
+    private fun getFormattedString(rules: Map<String, String>): String {
         return when (rules.size) {
             1 -> {
                 val key = rules.keys.first()
-                String.format(template, "{\"value\": \"$key\", \"tag\": \"${rules[key]}\"}")
+                String.format(BODY_ADD_TEMPLATE, "{\"value\": \"$key\", \"tag\": \"${rules[key]}\"}")
             }
 
             else -> {
@@ -146,7 +163,7 @@ open class TwitterV2StreamHelper(
                 for ((key, value) in rules) {
                     sb.append("{\"value\": \"$key\", \"tag\": \"$value\"},")
                 }
-                return String.format(template, sb.toString().substring(0, sb.length - 1))
+                return String.format(BODY_ADD_TEMPLATE, sb.toString().substring(0, sb.length - 1))
             }
         }
     }
@@ -183,9 +200,10 @@ open class TwitterV2StreamHelper(
 
     private fun createHttpRequest(httpRequest: HttpRequest, bearerToken: String): HttpRequestBase {
         val uriBuilder = URIBuilder(twitterProperties.twitterV2RulesBaseUrl)
-        var request = when (httpRequest) {
+        val request = when (httpRequest) {
             POST -> HttpPost(uriBuilder.build())
             GET -> HttpGet(uriBuilder.build())
+            else -> throw IllegalArgumentException("Not allowed argument as request")
         }
         request.setHeaders(
             arrayOf(
