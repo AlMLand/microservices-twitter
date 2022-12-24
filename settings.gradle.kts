6c544a3e2d12234b6cc91fe357719f27efe56446
Alex Morland <morlandalex@googlemail.com>
2022-12-25 00:20:46 +0100

    gradle : structure for kafka module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
 rootProject.name = "microservices-twitter"
 include("twitter-to-kafka-service")
 include("app-config")
+include("kafka")
+include("kafka:kafka-admin")
+findProject(":kafka:kafka-admin")?.name = "kafka-admin"
+include("kafka:kafka-model")
+findProject(":kafka:kafka-model")?.name = "kafka-model"
+include("kafka:kafka-producer")
+findProject(":kafka:kafka-producer")?.name = "kafka-producer"
