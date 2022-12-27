ab3fdf019a97800b389a864216f36e8e374995e1
Alex Morland <morlandalex@googlemail.com>
2022-12-27 22:54:55 +0100

    kafka-model : refactoring, moved the generated class from 'auto-generated' directory to the 'main/java'

--- kafka/kafka-model/build.gradle.kts
+++ kafka/kafka-model/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
 }
 
 // wenn man die files nicht im build(als 'generated source root') haben möchte, sondern direkt in 'java' ordner
-//tasks.withType<com.github.davidmc24.gradle.plugin.avro.GenerateAvroJavaTask> {
-//    setOutputDir(file("src/main/java/"))
-//}
+tasks.withType<com.github.davidmc24.gradle.plugin.avro.GenerateAvroJavaTask> {
+    source(file("src/main/resources/"))
+    setOutputDir(file("src/main/java/"))
+}
