4eb630146fca0455f218f3b76fcbf16c2042e625
Alex Morland <morlandalex@googlemail.com>
2022-12-29 23:10:05 +0100

    refactoring : changed from deprecated AdminClient abstract class to newer Admin interface

--- kafka/kafka-admin/src/main/kotlin/com/AlMLand/kafka/admin/configuration/KafkaAdminConfiguration.kt
+++ kafka/kafka-admin/src/main/kotlin/com/AlMLand/kafka/admin/configuration/KafkaAdminConfiguration.kt
@@ -2,7 +2,7 @@ package com.AlMLand.kafka.admin.configuration
 
 import com.AlMLand.kafkaAdmin.KafkaProperties
 import org.apache.kafka.clients.CommonClientConfigs
-import org.apache.kafka.clients.admin.AdminClient
+import org.apache.kafka.clients.admin.Admin
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.retry.annotation.EnableRetry
@@ -11,6 +11,6 @@ import org.springframework.retry.annotation.EnableRetry
 @Configuration
 class KafkaAdminConfiguration(private val kafkaProperties: KafkaProperties) {
     @Bean
-    fun adminClient(): AdminClient =
-        AdminClient.create(mapOf(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG to kafkaProperties.bootstrapServers))
+    fun kafkaAdmin(): Admin =
+        Admin.create(mapOf(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG to kafkaProperties.bootstrapServers))
 }
