7f69196e24a4421cb8cc50981ee2d813e184d410
Alex Morland <morlandalex@googlemail.com>
2023-01-15 22:12:04 +0100

    kafka-to-elasticsearch : add check to start of application, disabled autostart

--- kafka-to-elasticsearch-service/src/main/kotlin/com/AlMLand/elasticsearch/consumer/KafkaConsumer.kt
+++ kafka-to-elasticsearch-service/src/main/kotlin/com/AlMLand/elasticsearch/consumer/KafkaConsumer.kt
@@ -5,6 +5,8 @@ import com.AlMLand.kafka.avro.model.TwitterAvroModel
 import com.AlMLand.kafka.kafkaAdmin.KafkaProperties
 import org.apache.avro.specific.SpecificRecordBase
 import org.slf4j.LoggerFactory
+import org.springframework.boot.context.event.ApplicationStartedEvent
+import org.springframework.context.event.EventListener
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry
 import org.springframework.kafka.support.KafkaHeaders.*
@@ -24,9 +26,10 @@ sealed interface KafkaConsumer<K : Serializable, V : SpecificRecordBase> {
     ) : KafkaConsumer<Long, TwitterAvroModel> {
         companion object {
             private val logger = LoggerFactory.getLogger(this::class.java)
+            private const val TOPIC_LISTENER_ID = "twitter-topic-listener"
         }
 
-        @KafkaListener(id = "twitterTopicListener", topics = ["\${kafka-configuration.topic-name}"])
+        @KafkaListener(id = TOPIC_LISTENER_ID, topics = ["\${kafka-configuration.topic-name}"])
         override fun receive(
             @Payload messages: List<TwitterAvroModel>,
             @Header(RECEIVED_KEY) keys: List<Int>,
@@ -43,5 +46,18 @@ sealed interface KafkaConsumer<K : Serializable, V : SpecificRecordBase> {
                 offsets
             )
         }
+
+        @EventListener
+        fun startListening(event: ApplicationStartedEvent) =
+            kafkaAdminClient.checkTopicsCreated().let {
+                kafkaListenerEndpointRegistry
+                    .getListenerContainer(TOPIC_LISTENER_ID)?.start().also {
+                        logger.info(
+                            "Topics with names {} are ready for operations.",
+                            kafkaProperties.topicNamesToCreate
+                        )
+                    }
+                    ?: error("The listener container with id: $TOPIC_LISTENER_ID is not found")
+            }
     }
 }
