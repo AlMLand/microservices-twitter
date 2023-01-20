79bf554af7a6f82e5eae114f1634ad99920ecc3b
Alex Morland <morlandalex@googlemail.com>
2023-01-20 23:56:47 +0100

    refactoring

--- kafka-to-elasticsearch-service/src/main/kotlin/com/AlMLand/elasticsearch/consumer/KafkaConsumer.kt
+++ kafka-to-elasticsearch-service/src/main/kotlin/com/AlMLand/elasticsearch/consumer/KafkaConsumer.kt
@@ -9,14 +9,14 @@ import org.springframework.boot.context.event.ApplicationStartedEvent
 import org.springframework.context.event.EventListener
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry
-import org.springframework.kafka.support.KafkaHeaders.*
+import org.springframework.kafka.support.KafkaHeaders
 import org.springframework.messaging.handler.annotation.Header
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Service
 import java.io.Serializable
 
 sealed interface KafkaConsumer<K : Serializable, V : SpecificRecordBase> {
-    fun receive(messages: List<V>, keys: List<Int>, partitions: List<Int>, offsets: List<Long>)
+    fun receive(messages: List<V>, keys: List<K>, partitions: List<Int>, offsets: List<Long>)
 
     @Service
     class TwitterKafkaConsumer(
@@ -32,18 +32,14 @@ sealed interface KafkaConsumer<K : Serializable, V : SpecificRecordBase> {
         @KafkaListener(id = TOPIC_LISTENER_ID, topics = ["\${kafka-configuration.topic-name}"])
         override fun receive(
             @Payload messages: List<TwitterAvroModel>,
-            @Header(RECEIVED_KEY) keys: List<Int>,
-            @Header(RECEIVED_PARTITION) partitions: List<Int>,
-            @Header(OFFSET) offsets: List<Long>
+            @Header(KafkaHeaders.RECEIVED_KEY) keys: List<Long>,
+            @Header(KafkaHeaders.RECEIVED_PARTITION) partitions: List<Int>,
+            @Header(KafkaHeaders.OFFSET) offsets: List<Long>
         ) {
             logger.info(
                 """
-                {} number of message received with keys {}, partitions {} and offsets {}
-            """.trimIndent(),
-                messages.size,
-                keys,
-                partitions,
-                offsets
+                ${messages.size} number of message received with keys $keys, partitions $partitions and offsets $offsets
+            """.trimIndent()
             )
         }
 
@@ -53,8 +49,7 @@ sealed interface KafkaConsumer<K : Serializable, V : SpecificRecordBase> {
                 kafkaListenerEndpointRegistry
                     .getListenerContainer(TOPIC_LISTENER_ID)?.start().also {
                         logger.info(
-                            "Topics with names {} are ready for operations.",
-                            kafkaProperties.topicNamesToCreate
+                            "Topics with names ${kafkaProperties.topicNamesToCreate} are ready for operations."
                         )
                     }
                     ?: error("The listener container with id: $TOPIC_LISTENER_ID is not found")
