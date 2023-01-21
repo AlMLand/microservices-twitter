53d41d322cfd14df70943edcd0de5a93ad88186e
Alex Morland <morlandalex@googlemail.com>
2023-01-22 00:41:03 +0100

    docker : create kafka topic from docker compose file, programmatically solution stays in the code

--- twitter-to-kafka-service/src/main/kotlin/com/AlMLand/initialization/StreamInitializer.kt
+++ twitter-to-kafka-service/src/main/kotlin/com/AlMLand/initialization/StreamInitializer.kt
@@ -3,12 +3,15 @@ package com.AlMLand.initialization
 import com.AlMLand.kafka.admin.client.KafkaAdminClient
 import com.AlMLand.kafka.kafkaAdmin.KafkaProperties
 import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 
+@FunctionalInterface
 sealed interface StreamInitializer {
     fun init()
 
     @Component
+    @ConditionalOnProperty("twitter-to-kafka-service.kafka-initialization-programmatically")
     class KafkaStreamInitializer(
         private val kafkaProperties: KafkaProperties,
         private val kafkaAdminClient: KafkaAdminClient
